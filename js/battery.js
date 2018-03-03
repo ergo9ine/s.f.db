436855fcfff7e3d8f4640f17a9851bdef09662de
고승현 <kosehy@gmail.com>
2018-03-04 00:40:26 +0900

    Update battery

--- js/battery.js
+++ js/battery.js
@@ -1,3 +1,5 @@
+// JavaScript Document
+// battery
 document.getElementById("1com").addEventListener("keyup", sumComfort);
 document.getElementById("2com").addEventListener("keyup", sumComfort);
 document.getElementById("3com").addEventListener("keyup", sumComfort);
@@ -8,14 +10,13 @@ document.getElementById("7com").addEventListener("keyup", sumComfort);
 document.getElementById("8com").addEventListener("keyup", sumComfort);
 document.getElementById("9com").addEventListener("keyup", sumComfort);
 document.getElementById("10com").addEventListener("keyup",sumComfort);
-var arr = ["1com", "2com", "3com", "4com", "5com", "6com", "7com", "8com", "9com", "10com"];
-var a = 0;
+var arrCom = ["1com", "2com", "3com", "4com", "5com", "6com", "7com", "8com", "9com", "10com"];
+var totalCom = 0;
 var i;
 function sumComfort() {
-	"use strict";
-    for (i = 0; i < arr.length; i++)
+    for (i = 0; i < arrCom.length; i++)
     {
-    	a = a + Number(document.getElementById(arr[i]).value);
+    	totalCom = totalCom + Number(document.getElementById(arrCom[i]).value);
     }
-	document.getElementById("tCom").innerText = a;
+	document.getElementById("tCom").innerText = totalCom;
 }
