b35a3b8727cf79d3fb7344947cf4ac7199e36148
GFDB <ergo9ine@gmail.com>
2018-03-16 23:36:18 +0900

    v0.1.2.4.5 temp mobile

--- js/index.js
+++ js/index.js
@@ -88,14 +88,6 @@ $(document).ready(function() {
 	});
 	var	filterizd =	$('#gun').filterizr({
 	controlsSelector:'.fc',
-	filterOutCss:{
-		opacity:0,
-		transform:'scale(0.75)'
-	},
-	filterInCss:{
-		opacity:1,
-		transform:'scale(1)'
-	}
 	});
 	$('.fo li').click(function() {
 		var sortBy = $('.so').val();
