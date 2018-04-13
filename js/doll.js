bdd42601acb4d1cb6ce8312b5fd33cc5fb4aecde
ergo9ine <ergo9ine@gmail.com>
2018-04-13 21:52:46 +0900

    v0.1.3.2.2 doll.html bug fix

--- js/doll.js
+++ js/doll.js
@@ -150,41 +150,6 @@ function loadComplete(){
 				var w3img='<img class="w3-image">'
 				$('div.w3-row:nth-child(8)>div:nth-child(1)').append(w3img)
 				$("div.w3-row>div:nth-child(1)>img:nth-child(1)").attr("src",cimg);
-				$(".skintag>button").click(function(){
-					var SkinsB=$(this).index();
-					switch (SkinsB){
-					case 0:
-						Skinbutton();
-					break;
-					case 1:
-						$(".w3-image").attr('src',cimg);
-					break;
-					case 2:
-						$(".w3-image").attr('src',idir+doll.id+'_1.png');
-					break;
-					case 3:
-						$(".w3-image").attr('src',idir+doll.id+'_2.png');
-					break;
-					case 4:
-						$(".w3-image").attr('src',idir+doll.id+'_3.png');
-					break;
-					case 5:
-						$(".w3-image").attr('src',idir+doll.id+'_4.png');
-					break;
-					case 6:
-						$(".w3-image").attr('src',idir+doll.id+'_5.png');
-					break;
-					case 7:
-						$(".w3-image").attr('src',idir+doll.id+'_6.png');
-					break;
-					case 8:
-						$(".w3-image").attr('src',idir+doll.id+'_7.png');
-					break;
-					case 9:
-						$(".w3-image").attr('src',idir+doll.id+'_8.png');
-					break;
-					};
-				});
 				$("div.w3-left-align:nth-child(1)>div:nth-child(1)>div:nth-child(3)").html(doll.voice);
 				$("div.w3-left-align:nth-child(1)>div:nth-child(3)>div:nth-child(3)").html(doll.illust);
 				$("div.w3-display-container:nth-child(5)>div:nth-child(3)").html(doll.name);
@@ -211,6 +176,41 @@ function loadComplete(){
 			$(".w3-image").attr('src',idir+imgsrc+'_d.png');
 		}
 	}
+	$(".skintag>button").click(function(){
+		var SkinsB=$(this).index();
+		switch (SkinsB){
+		case 0:
+			Skinbutton();
+		break;
+		case 1:
+			$(".w3-image").attr('src',cimg);
+		break;
+		case 2:
+			$(".w3-image").attr('src',idir+doll.id+'_1.png');
+		break;
+		case 3:
+			$(".w3-image").attr('src',idir+doll.id+'_2.png');
+		break;
+		case 4:
+			$(".w3-image").attr('src',idir+doll.id+'_3.png');
+		break;
+		case 5:
+			$(".w3-image").attr('src',idir+doll.id+'_4.png');
+		break;
+		case 6:
+			$(".w3-image").attr('src',idir+doll.id+'_5.png');
+		break;
+		case 7:
+			$(".w3-image").attr('src',idir+doll.id+'_6.png');
+		break;
+		case 8:
+			$(".w3-image").attr('src',idir+doll.id+'_7.png');
+		break;
+		case 9:
+			$(".w3-image").attr('src',idir+doll.id+'_8.png');
+		break;
+		};
+	});
 	$(".xfunc").click(()=>{
 		togglecon();
 		$(".w3-image").remove();
