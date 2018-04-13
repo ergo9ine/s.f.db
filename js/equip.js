c98c796bb1df5f56e392b6d3496f3bbc2b28fb8e
ergo9ine <ergo9ine@gmail.com>
2018-04-13 20:42:53 +0900

    v0.1.3.2.1.2 equip.html bug fix

--- js/equip.js
+++ js/equip.js
@@ -3,24 +3,26 @@ $(document).ready(()=>{
 		contentType:'application/json',
 		dataType:'json',
 		success:result=>{
-			itemcon = '<div class="w3-hover-shadow equip item-content">';
-			var allCharacters = $.map(result,(equip,index)=>{
-			character = $('<div class="item" data-time="'+equip.time+'" data-type="'+equip.type+'" data-rarity="'+equip.rarity+'"></div>');
-			equipcon = '<p class="name pofa f125 er'+equip.rarity+'">'+equip.name+'</p><i class="star r'+equip.rarity+'"></i><i class="equip	equip_info_cage_'+equip.rarity+' incage"></i><i class="equip bg_'+equip.rarity+' bg"></i><i class="equip '+equip.type+'_'+equip.rarity+' etype"></i><img src="../img/equip/'+equip.img+'.png" class="icon2" alt="icon"><div class="tag">'+equip.tag+'/'+equip.time+'</div>';
-			$(character).append(itemcon).find(".item-content").html(equipcon);character;
-		});
-		$('.grid').append(allCharacters)
+			itemcon='<div class="w3-hover-shadow equip item-content">';
+			var allCharacters=$.map(result,(equip,index)=>{
+				character = $('<div class="item" data-time="'+equip.time+'" data-type="'+equip.type+'" data-rarity="'+equip.rarity+'"></div>');
+				equipcon = '<p class="name pofa f125 er'+equip.rarity+'">'+equip.name+'</p><i class="star r'+equip.rarity+'"></i><i class="equip	equip_info_cage_'+equip.rarity+' incage"></i><i class="equip bg_'+equip.rarity+' bg"></i><i class="equip '+equip.type+'_'+equip.rarity+' etype"></i><img src="../img/equip/'+equip.img+'.png" class="icon2" alt="icon"><div class="tag">'+equip.tag+'/'+equip.time+'</div>';
+				$(character).append(itemcon).find(".item-content").html(equipcon);character;
+				return character;
+			});
+			$('.grid').append(allCharacters);
+			loadComplete();
 		},
 		error:(request,errorType,errorMessage)=>{alert('Error:'+errorType+' With message:'+errorMessage)},
 		timeout:3000
 	});
-}).ajaxStop(()=>{loadComplete()});
+});
 function sortrarity(){grid.sort('rarity')};
 function sorttime(){grid.sort('time')};
 function sorttype(){grid.sort('type')};
 $("select").change(()=>{
 	$("select:focus option:selected").each(function(){
-		var query = $(this).text()
+		var query=$(this).text()
 		switch (query){
 		case "기본":
 			new Muuri('.grid',{sordData:null});
@@ -39,7 +41,7 @@ $("select").change(()=>{
 });
 function loadComplete(){
 	$('.grid').removeClass('w3-hide');
-	grid = new Muuri('.grid',{
+	grid=new Muuri('.grid',{
 		sortData:{
 			time:(item,element)=>element.getAttribute('data-time'),
 			type:(item,element)=>element.getAttribute('data-type').toUpperCase(),
@@ -61,7 +63,7 @@ function loadComplete(){
 	});
 	function active(){$('.fc').removeClass('active')};
 	$(".fc").click(function(){
-		var query = $(this).text();
+		var query=$(this).text();
 		switch (query){
 		case "2성":
 			grid.filter('[data-rarity="2"]')
